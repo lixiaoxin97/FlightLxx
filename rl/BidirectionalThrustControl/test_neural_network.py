475e64aed42203d245a5e382c19fb0f93a9032e3
lixiaoxin97 <lixiaoxin97@foxmail.com>
2024-12-09 22:18:46 +0800

    bidirectional thrust control

--- rl/BidirectionalThrustControl/test_neural_network.py
+++ rl/BidirectionalThrustControl/test_neural_network.py
@@ -11,25 +11,57 @@ def btc_test_model(env, model_1, model_2, render=False):
     gs = gridspec.GridSpec(5, 12)
     #
     ax_x = fig.add_subplot(gs[0, 0:4])
+    ax_x.set_xlabel('t(s)')
+    ax_x.set_ylabel('pos_x(m)')
     ax_y = fig.add_subplot(gs[0, 4:8])
+    ax_y.set_xlabel('t(s)')
+    ax_y.set_ylabel('pos_y(m)')
     ax_z = fig.add_subplot(gs[0, 8:12])
+    ax_z.set_xlabel('t(s)')
+    ax_z.set_ylabel('pos_z(m)')
     #
     ax_dx = fig.add_subplot(gs[1, 0:4])
+    ax_dx.set_xlabel('t(s)')
+    ax_dx.set_ylabel('vel_x(m/s)')
     ax_dy = fig.add_subplot(gs[1, 4:8])
+    ax_dy.set_xlabel('t(s)')
+    ax_dy.set_ylabel('vel_y(m/s)')
     ax_dz = fig.add_subplot(gs[1, 8:12])
+    ax_dz.set_xlabel('t(s)')
+    ax_dz.set_ylabel('vel_z(m/s)')
     #
     ax_euler_x = fig.add_subplot(gs[2, 0:4])
+    ax_euler_x.set_xlabel('t(s)')
+    ax_euler_x.set_ylabel('euler_x(rad)')
     ax_euler_y = fig.add_subplot(gs[2, 4:8])
+    ax_euler_y.set_xlabel('t(s)')
+    ax_euler_y.set_ylabel('euler_y(rad)')
     ax_euler_z = fig.add_subplot(gs[2, 8:12])
+    ax_euler_z.set_xlabel('t(s)')
+    ax_euler_z.set_ylabel('euler_z(rad)')
     #
     ax_euler_vx = fig.add_subplot(gs[3, 0:4])
+    ax_euler_vx.set_xlabel('t(s)')
+    ax_euler_vx.set_ylabel('omega_x(rad/s)')
     ax_euler_vy = fig.add_subplot(gs[3, 4:8])
+    ax_euler_vy.set_xlabel('t(s)')
+    ax_euler_vy.set_ylabel('omega_y(rad/s)')
     ax_euler_vz = fig.add_subplot(gs[3, 8:12])
+    ax_euler_vz.set_xlabel('t(s)')
+    ax_euler_vz.set_ylabel('omega_z(rad/s)')
     #
     ax_action0 = fig.add_subplot(gs[4, 0:3])
+    ax_action0.set_xlabel('t(s)')
+    ax_action0.set_ylabel('act_1(N)')
     ax_action1 = fig.add_subplot(gs[4, 3:6])
+    ax_action1.set_xlabel('t(s)')
+    ax_action1.set_ylabel('act_2(N)')
     ax_action2 = fig.add_subplot(gs[4, 6:9])
+    ax_action2.set_xlabel('t(s)')
+    ax_action2.set_ylabel('act_3(N)')
     ax_action3 = fig.add_subplot(gs[4, 9:12])
+    ax_action3.set_xlabel('t(s)')
+    ax_action3.set_ylabel('act_4(N)')
 
     max_ep_length = env.max_episode_steps
     num_rollouts = 1
@@ -79,28 +111,28 @@ def btc_test_model(env, model_1, model_2, render=False):
         ax_y.step(t * 0.02, pos[:, 1], color="C{0}".format(
             11), label="trail: {0}".format(n_roll))
         ax_z.step(t * 0.02, pos[:, 2] + 5 , color="C{0}".format(
-            11), label="pos [x, y, z] -- BTC".format(n_roll))
+            11), label="position -- BTC".format(n_roll))
         #
         ax_dx.step(t * 0.02, dpos[:, 0], color="C{0}".format(
             11), label="trail: {0}".format(n_roll))
         ax_dy.step(t * 0.02, dpos[:, 1], color="C{0}".format(
             11), label="trail: {0}".format(n_roll))
         ax_dz.step(t * 0.02, dpos[:, 2], color="C{0}".format(
-            11), label="vel [x, y, z] -- BTC".format(n_roll))
+            11), label="velocity -- BTC".format(n_roll))
         #
         ax_euler_x.step(t * 0.02, euler[:, -1], color="C{0}".format(
             11), label="trail: {0}".format(n_roll))
         ax_euler_y.step(t * 0.02, euler[:, 0], color="C{0}".format(
             11), label="trail :{0}".format(n_roll))
         ax_euler_z.step(t * 0.02, euler[:, 1], color="C{0}".format(
-            11), label="euler [x, y, z] -- BTC".format(n_roll))
+            11), label="orientation -- BTC".format(n_roll))
         #
         ax_euler_vx.step(t * 0.02, deuler[:, 0], color="C{0}".format(
             11), label="trail: {0}".format(n_roll))
         ax_euler_vy.step(t * 0.02, deuler[:, 1], color="C{0}".format(
             11), label="trail :{0}".format(n_roll))
         ax_euler_vz.step(t * 0.02, deuler[:, 2], color="C{0}".format(
-            11), label="omgea [x, y, z] -- BTC".format(n_roll))
+            11), label="bodyrate -- BTC".format(n_roll))
         # NP2G
         ax_action0.step(t * 0.02, actions[:, 0] * 0.78 * 9.81 / 2, color="C{0}".format(
             11), label="trail: {0}".format(n_roll))
@@ -109,7 +141,7 @@ def btc_test_model(env, model_1, model_2, render=False):
         ax_action2.step(t * 0.02, actions[:, 2] * 0.78 * 9.81 / 2, color="C{0}".format(
             11), label="trail: {0}".format(n_roll))
         ax_action3.step(t * 0.02, actions[:, 3] * 0.78 * 9.81 / 2, color="C{0}".format(
-            11), label="act [1, 2, 3, 4] -- BTC".format(n_roll))
+            11), label="action -- BTC".format(n_roll))
 
 
 
@@ -161,28 +193,28 @@ def btc_test_model(env, model_1, model_2, render=False):
         ax_y.step(t * 0.02, pos[:, 1], color="C{0}".format(
             n_roll), label="trail: {0}".format(n_roll))
         ax_z.step(t * 0.02, pos[:, 2] + 5 , color="C{0}".format(
-            n_roll), label="pos [x, y, z] -- OPTC".format(n_roll))
+            n_roll), label="position -- OPTC".format(n_roll))
         #
         ax_dx.step(t * 0.02, dpos[:, 0], color="C{0}".format(
             n_roll), label="trail: {0}".format(n_roll))
         ax_dy.step(t * 0.02, dpos[:, 1], color="C{0}".format(
             n_roll), label="trail: {0}".format(n_roll))
         ax_dz.step(t * 0.02, dpos[:, 2], color="C{0}".format(
-            n_roll), label="vel [x, y, z] -- OPTC".format(n_roll))
+            n_roll), label="velocity -- OPTC".format(n_roll))
         #
         ax_euler_x.step(t * 0.02, euler[:, -1], color="C{0}".format(
             n_roll), label="trail: {0}".format(n_roll))
         ax_euler_y.step(t * 0.02, euler[:, 0], color="C{0}".format(
             n_roll), label="trail :{0}".format(n_roll))
         ax_euler_z.step(t * 0.02, euler[:, 1], color="C{0}".format(
-            n_roll), label="euler [x, y, z] -- OPTC".format(n_roll))
+            n_roll), label="orientation -- OPTC".format(n_roll))
         #
         ax_euler_vx.step(t * 0.02, deuler[:, 0], color="C{0}".format(
             n_roll), label="trail: {0}".format(n_roll))
         ax_euler_vy.step(t * 0.02, deuler[:, 1], color="C{0}".format(
             n_roll), label="trail :{0}".format(n_roll))
         ax_euler_vz.step(t * 0.02, deuler[:, 2], color="C{0}".format(
-            n_roll), label="omgea [x, y, z] -- OPTC".format(n_roll))
+            n_roll), label="bodyrate -- OPTC".format(n_roll))
         #
         ax_action0.step(t * 0.02, actions[:, 0] * 0.78 * 9.81 / 2, color="C{0}".format(
             n_roll), label="trail: {0}".format(n_roll))
@@ -191,7 +223,7 @@ def btc_test_model(env, model_1, model_2, render=False):
         ax_action2.step(t * 0.02, actions[:, 2] * 0.78 * 9.81 / 2, color="C{0}".format(
             n_roll), label="trail: {0}".format(n_roll))
         ax_action3.step(t * 0.02, actions[:, 3] * 0.78 * 9.81 / 2, color="C{0}".format(
-            n_roll), label="act [1, 2, 3, 4] -- OPTC".format(n_roll))
+            n_roll), label="action -- OPTC".format(n_roll))
 
     
 
@@ -200,11 +232,11 @@ def btc_test_model(env, model_1, model_2, render=False):
 
     if render:
         env.disconnectUnity()
-    ax_z.legend()
-    ax_dz.legend()
-    ax_euler_z.legend()
-    ax_euler_vz.legend()
-    ax_action3.legend()
+    ax_z.legend(loc='lower right')
+    ax_dz.legend(loc='lower right')
+    ax_euler_z.legend(loc='lower right')
+    ax_euler_vz.legend(loc='lower right')
+    ax_action3.legend(loc='lower right')
     #
     plt.tight_layout()
     plt.show()
